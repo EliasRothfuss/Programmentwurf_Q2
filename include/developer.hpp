fedbc5daa346b4777f45b4d209f3282ef7019825
Elias Rothfuss <eliasrothfuss@gmail.com>
2023-05-27 01:55:53 +0200

    Implemented Developer Class (ToDo: operater << )

--- include/developer.hpp
+++ include/developer.hpp
@@ -1,5 +1,28 @@
 #ifndef DEVELOPER_H_FJFSUBGBTBITDAD
 #define DEVELOPER_H_FJFSUBGBTBITDAD
 
+#include <string>
+
+class developer
+{
+   private:
+    std::string name;
+    std::string alias;
+
+    virtual void solve_problem() const = 0;
+
+   public:
+    developer(std::string _name, std::string _alias);
+
+    static void drink_coffee();
+
+    std::string get_name();
+    std::string get_alias();
+
+    /*
+    Außerhalb der Klasse soll zusätzlich der Stream-Operator (<<) so implementiert werden, dass die Klasse Developer
+    direkt mit std::cout verwendet werden kann
+    */
+};
 
 #endif
--- src/developer.cpp
+++ src/developer.cpp
@@ -1,2 +1,25 @@
 #include "developer.hpp"
 
+#include <iostream>
+#include <string>
+
+developer::developer(std::string _name, std::string _alias)
+{
+    developer::name = _name;
+    developer::alias = _alias;
+}
+
+void developer::drink_coffee()
+{
+    std::cout << "I need some Coffee" << std::endl;
+}
+
+std::string developer::get_name()
+{
+    return developer::name;
+}
+
+std::string developer::get_alias()
+{
+    return developer::alias;
+}
