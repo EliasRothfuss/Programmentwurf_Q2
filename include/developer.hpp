10db129157dfb2c2475a33bdc25246c5d8fc2c7d
Marco-Zagst <122740908+Marco-Zagst@users.noreply.github.com>
2023-05-27 20:16:17 +0000

    Added junior and senior classes

--- include/developer.hpp
+++ include/developer.hpp
@@ -5,13 +5,11 @@
 
 class developer
 {
-   private:
+   public:
+
     std::string name;
     std::string alias;
 
-    virtual void solve_problem() const = 0;
-
-   public:
     developer(std::string _name, std::string _alias);
 
     static void drink_coffee();
@@ -19,10 +17,29 @@ class developer
     std::string get_name();
     std::string get_alias();
 
+    virtual void solve_problem() const = 0;
+
     /*
     Außerhalb der Klasse soll zusätzlich der Stream-Operator (<<) so implementiert werden, dass die Klasse Developer
     direkt mit std::cout verwendet werden kann
     */
+};
+
+class senior_developer : public developer
+{
+    public:
+    senior_developer(std::string _name, std::string _alias):developer(_name , _alias);
+    
+    void solve_problem() const override;
+};
+
+class junior_developer : public developer
+{
+    public:
+    junior_developer(std::string _name, std::string _alias):developer(_name , _alias);
+    
+    void solve_problem() const override;
+
 };
 
 #endif
