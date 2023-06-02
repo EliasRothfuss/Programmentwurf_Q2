629585ac63508e60ad9ae8289af0d5d195b99670
Marius Merkle <122784296+merklema@users.noreply.github.com>
2023-06-02 09:06:49 +0000

    First G Tests. Already working, but not finished

--- test/developer_test.cpp
+++ test/developer_test.cpp
@@ -1,2 +1,17 @@
 #include "gtest/gtest.h"  // include the gtest functions & macros
 #include "developer.hpp"
+
+#define test_name Kaffeemaschiene;
+
+TEST(DeveloperTest, ConstructorTest) {
+    JuniorDeveloper dev("FFF", "DDD");
+    ASSERT_EQ(dev.get_name(), "FFF");
+    ASSERT_EQ(dev.get_alias(), "DDD");
+}
+
+
+TEST(SeniorTest, ConstructorTest) {
+    SeniorDeveloper dev("test_name", "test_name");
+    ASSERT_EQ(dev.get_name(), "test_name");
+    ASSERT_STREQ(dev.get_alias().c_str(), "test_name"); //to do, STREQ or EQ Funktion, decide whats better
+}
