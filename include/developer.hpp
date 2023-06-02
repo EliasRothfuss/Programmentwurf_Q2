d2167359d4fb67c5a24b39b9c9c4861e9f925417
Elias Rothfuss <eliasrothfuss@gmail.com>
2023-06-02 12:07:30 +0200

    Finished solve_problem Method

--- include/developer.hpp
+++ include/developer.hpp
@@ -8,11 +8,12 @@ class Developer  // Base Class Developer
    protected:
     std::string name_;   // Name of the developer
     std::string alias_;  // Alias of the developer
+    void solve_problem_template() const;
 
    public:
     Developer(std::string name, std::string alias);  // Constructor to set the name and alias of the Developer
 
-    static void drink_coffee();  // Imported method ;)
+    static void drink_coffee();              // Imported method ;)
     virtual void solve_problem() const = 0;  // vi
 
     std::string get_name() const;   // Get the name of the Developer
--- src/developer.cpp
+++ src/developer.cpp
@@ -3,6 +3,8 @@
 #include <iostream>
 #include <string>
 
+#define spacer std::cout << "------------------" << std::endl
+
 Developer::Developer(std::string name, std::string alias)  // Base Class Constructor
 {
     Developer::name_ = name;    // Set Developer Name
@@ -24,10 +26,16 @@ std::string Developer::get_alias() const  // Get the alias of the Developer
     return Developer::alias_;
 }
 
+void Developer::solve_problem_template() const
+{
+    std::cout << "Solving a problem:" << std::endl;
+    std::cout << *this;
+}
+
 auto operator<<(std::ostream& stream, const Developer& dev)
     -> std::ostream&  // Overloaded operator << to output Developer name and alias
 {
-    stream << "Name: " << dev.get_name() << " Alias: " << dev.get_alias() << std::endl;
+    stream << "Name: " << dev.get_name() << "\nAlias: " << dev.get_alias() << std::endl;
     return stream;
 }
 
@@ -41,14 +49,20 @@ JuniorDeveloper::JuniorDeveloper(std::string name, std::string alias)
 {
 }
 
-void JuniorDeveloper::solve_problem()
-    const  // Implementation of the solve_problem method for the JuniorDeveloper class
+void JuniorDeveloper::solve_problem() const  // Implementation of the solve_problem method for the JuniorDeveloper class
 {
+    spacer;
+    this -> solve_problem_template();
+    std::cout << "Puh, that was tough" << std::endl;
     Developer::drink_coffee();
+    spacer;
 }
 
-void SeniorDeveloper::solve_problem()
-    const  // Implementation of the solve_problem method for the SeniorDeveloper class
+void SeniorDeveloper::solve_problem() const  // Implementation of the solve_problem method for the SeniorDeveloper class
 {
+    spacer;
+    this -> solve_problem_template();
+    std::cout << "Ha, that was EZ!" << std::endl;
     Developer::drink_coffee();
+    spacer;
 }
